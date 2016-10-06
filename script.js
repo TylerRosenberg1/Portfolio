acd4bdcbb46346a24b16d93200ff4bbd2ff9b2e5
Tyler Rosenberg <tylerrosenberg1@gmail.com>
2016-10-05 22:25:40 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    scrollBar: true
+  });
+
+  $('#section-one h1').fadeIn({
+    duration: 2000
+  });
+
+  $('#section-one p').fadeIn({
+    duration: 3000
+  });
+});
